eab98b49be34634b0923b44456d8cd235a0593c3
Lakshmi Chaitanya Rayala <lakshmi.rayala@agcocorp.com>
2020-06-16 23:28:39 -0500

    initialized a spring boot hibernate rest api project that does CRUD operations using Postgres database.

---
+++ src/test/java/com/restapi/labs/SpringBootHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restapi.labs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
